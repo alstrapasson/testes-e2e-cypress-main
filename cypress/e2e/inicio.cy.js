ea6f33e97878ecd4ffcdfd451b8c86a4f924835d
alstrapasson <alstrapasson@gmail.com>
2023-10-03 22:17:22 -0300

    uso do gancho para acessar a url apenas uma vez a cada teste

--- cypress/e2e/inicio.cy.js
+++ cypress/e2e/inicio.cy.js
@@ -1,6 +1,9 @@
 describe('Pagina Inicial', () => {
-  it('Deve renderizar o h1 com o texto correto', () => {
+  beforeEach(()=>
     cy.visit('http://localhost:3000/')
+  )
+  it('Deve renderizar o h1 com o texto correto', () => {
+    
     //cy.get('h1').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
     cy.getByData('titulo-principal').contains(
       'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!'
