e465339de137427d0543c11ae3a08bbe53eb2290
alstrapasson <alstrapasson@gmail.com>
2023-10-03 22:15:31 -0300

    comandos personalizados!

--- cypress/e2e/inicio.cy.js
+++ cypress/e2e/inicio.cy.js
@@ -2,6 +2,8 @@ describe('Pagina Inicial', () => {
   it('Deve renderizar o h1 com o texto correto', () => {
     cy.visit('http://localhost:3000/')
     //cy.get('h1').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
-    cy.get('[data-test="titulo-principal"]').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+    cy.getByData('titulo-principal').contains(
+      'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!'
+    );
   })
 })
